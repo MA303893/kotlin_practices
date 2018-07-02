79fefc7d0efc62205d7c61b6efe5d862cbf340ee
Manish Singh <manish.singh@jifflenow.com>
2018-07-02 15:51:14 +0530

    Manish Singh : added constructor example

---
+++ src/ConstructorEg.kt
@@ -0,0 +1,17 @@
+class Person(name: String) {
+    var name = ""
+    init {
+        this.name = name
+    }
+
+    constructor() : this(""){
+
+    }
+    fun getPersonDetails() : String {
+        return this.name
+    }
+}
+fun main(args: Array<String>) {
+    val person = Person("Manish Singh")
+    println(person)
+}
