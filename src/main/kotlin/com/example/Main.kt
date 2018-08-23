f3c62e6f3e7e592b8817ac3c14a3c930fa157a5e
Manish Singh <manish.singh@jifflenow.com>
2018-08-23 10:10:29 +0530

    added log4j2 configs

--- src/main/kotlin/com/example/Main.kt
+++ src/main/kotlin/com/example/Main.kt
@@ -1,5 +1,11 @@
 package com.example
 
+import org.jdbi.v3.core.Handle
+import org.jdbi.v3.core.Jdbi
+
 fun main(args: Array<String>){
     println("Hello")
+    val jdbi = Jdbi.create("jdbc:postgresql://localhost/kotlin_jpa","kotlin_dbuser","kotlin_dbpwd")
+    val handle : Handle = jdbi.open()
+
 }
